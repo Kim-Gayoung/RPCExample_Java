4b9eaa2605df22ad67cbb7cab77f77c28949dd95
Gayoung Kim <qrkdudq4301@naver.com>
2019-02-24 19:46:04 +0900

    systemF의 testcase를 모두 돌려볼 수 있는 JUnitTestCase 생성

---
+++ src/com/example/systemf/test/TypeCheckTest.java
@@ -0,0 +1,43 @@
+package com.example.systemf.test;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.example.lib.LexerException;
+import com.example.lib.ParserException;
+import com.example.systemf.TyEnv;
+import com.example.systemf.TypeCheckException;
+import com.example.systemf.TypeChecker;
+import com.example.systemf.ast.TopLevel;
+import com.example.systemf.ast.Type;
+import com.example.systemf.parser.Parser;
+
+public class TypeCheckTest {
+	Parser parser;
+	
+	@Test
+	public void test() throws IOException, ParserException, LexerException, TypeCheckException {
+		parser = new Parser();
+		String directory = System.getProperty("user.dir");
+		recursiveRead(new File(directory + "/testcase/examples/systemF/"));
+	}
+	
+	public void recursiveRead(File file) throws ParserException, IOException, LexerException, TypeCheckException {
+		File[] listOfFiles = file.listFiles();
+		
+		for(File f: listOfFiles) {
+			if (f.isFile()) {
+				System.out.println("\n" + f);
+				FileReader fileReader = new FileReader(f);
+				TopLevel ex1 = (TopLevel) parser.Parsing(fileReader);
+				Type ty1 = TypeChecker.checkTopLevel(ex1, new TyEnv());
+				System.out.println(ex1);
+				System.out.println("Type: " + ty1.toString());
+			}
+		}
+	}
+
+}
