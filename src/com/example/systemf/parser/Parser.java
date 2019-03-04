4541c6d532543de1f7d5231a6435dab743af1f41
Gayoung Kim <qrkdudq4301@naver.com>
2019-03-04 12:38:22 +0900

    SystemF의 example에 모두 main 추가

--- src/com/example/systemf/parser/Parser.java
+++ src/com/example/systemf/parser/Parser.java
@@ -2,6 +2,7 @@ package com.example.systemf.parser;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
 
 import com.example.lib.CommonParserUtil;
 import com.example.lib.LexerException;
@@ -30,45 +31,89 @@ import com.example.systemf.ast.Unit;
 import com.example.systemf.ast.UnitType;
 import com.example.systemf.ast.Var;
 import com.example.systemf.ast.VarType;
+import com.example.utils.TripleTup;
 
 public class Parser {
 	private CommonParserUtil<Token> pu;
 	private static int n = 1;
-	
+
 	public Parser() throws IOException {
 		pu = new CommonParserUtil<Token>();
-		
+
 		new Lexer(pu);
-		
+
 		pu.ruleStartSymbol("TopLevel'");
-		pu.rule("TopLevel' -> TopLevel", () -> { return pu.get(1); });
-		pu.rule("TopLevel -> LExpr", () -> {
-			return (Term) pu.get(1);
+		pu.rule("TopLevel' -> TopLevel", () -> {
+			System.out.println("TopLevel'");
+			ArrayList<TripleTup<String, Type, Term>> list = (ArrayList<TripleTup<String, Type, Term>>) pu.get(1);
+			TripleTup<String, Type, Term> mainTerm = null;
+
+			for (TripleTup<String, Type, Term> t : list) {
+				String id = t.getFirst();
+
+				if (id.equals("main")) {
+					mainTerm = t;
+					list.remove(mainTerm);
+				}
+			}
+			try {
+				if (mainTerm == null)
+
+					throw new ParserException("Program must have main function");
+
+				else {
+					Term main = mainTerm.getThird();
+					Term ret = main;
+
+					for (int i = list.size() - 1; i >= 0; i--) {
+						TripleTup<String, Type, Term> t = list.get(i);
+
+						if (t.getSecond() != null)
+							ret = new Let(t.getFirst(), t.getSecond(), t.getThird(), ret);
+						else
+							ret = new Let(t.getFirst(), t.getThird(), ret);
+					}
+
+					return ret;
+				}
+			}
+			catch (ParserException e) {
+				e.printStackTrace();
+				return null;
+			}
 		});
 		pu.rule("TopLevel -> id OptType = LExpr", () -> {
 			String id = pu.getText(1);
-			Type ty = pu.get(2) != null? (Type) pu.get(2) : null;
+			Type ty = pu.get(2) != null ? (Type) pu.get(2) : null;
 			Term t1 = (Term) pu.get(4);
-			
-			return new Let(id, ty, t1, new Var(id));
+
+			TripleTup<String, Type, Term> top = new TripleTup<>(id, ty, t1);
+
+			ArrayList<TripleTup<String, Type, Term>> list = new ArrayList<>();
+			list.add(top);
+
+			return list;
 		});
 		pu.rule("TopLevel -> id OptType = LExpr ; TopLevel", () -> {
 			String id = pu.getText(1);
-			Type ty = pu.get(2) != null? (Type) pu.get(2) : null;
+			Type ty = pu.get(2) != null ? (Type) pu.get(2) : null;
 			Term t1 = (Term) pu.get(4);
-			Term next = (Term) pu.get(6);
-			
-			if (ty != null)
-				return new Let(id, ty, t1, next);
-			else
-				return new Let(id, t1, next);
-		}); 
+			ArrayList<TripleTup<String, Type, Term>> next = (ArrayList<TripleTup<String, Type, Term>>) pu.get(6);
+
+			TripleTup<String, Type, Term> top = new TripleTup<>(id, ty, t1);
+
+			ArrayList<TripleTup<String, Type, Term>> list = new ArrayList<>();
+			list.add(top);
+			list.addAll(next);
+
+			return list;
+		});
 		pu.rule("LExpr -> lam OptLoc id OptType . LExpr", () -> {
-			Location loc = pu.get(2) != null? (Location) pu.get(2) : null;
+			Location loc = pu.get(2) != null ? (Location) pu.get(2) : null;
 			String id = pu.getText(3);
-			Type ty = pu.get(4) != null? (Type) pu.get(4) : null;
+			Type ty = pu.get(4) != null ? (Type) pu.get(4) : null;
 			Term body = (Term) pu.get(6);
-			
+
 			if (ty != null)
 				return new Lam(loc, id, ty, body);
 			else
@@ -77,7 +122,7 @@ public class Parser {
 		pu.rule("LExpr -> tylam id . LExpr", () -> {
 			String strTy = pu.getText(2);
 			Term body = (Term) pu.get(4);
-			
+
 			return new Tylam(new VarType(strTy), body);
 		});
 		pu.rule("LExpr -> let id OptType = LExpr in LExpr end", () -> {
@@ -85,7 +130,7 @@ public class Parser {
 			Type ty = pu.get(3) != null ? (Type) pu.get(3) : null;
 			Term t1 = (Term) pu.get(5);
 			Term t2 = (Term) pu.get(7);
-			
+
 			if (ty != null)
 				return new Let(id, ty, t1, t2);
 			else
@@ -94,38 +139,50 @@ public class Parser {
 		pu.rule("LExpr -> if Expr then LExpr else LExpr", () -> {
 			return new If((Term) pu.get(2), (Term) pu.get(4), (Term) pu.get(6));
 		});
-		pu.rule("LExpr -> Expr", () -> { return pu.get(1); });
+		pu.rule("LExpr -> Expr", () -> {
+			return pu.get(1);
+		});
 
 		pu.rule("Expr -> Expr Term", () -> {
 			return new App((Term) pu.get(1), (Term) pu.get(2));
 		});
 		pu.rule("Expr -> Expr ^ id ^ Term", () -> {
 			Location loc = getLoc(pu.getText(3));
-		
+
 			return new App((Term) pu.get(1), (Term) pu.get(5), new LocType(loc));
 		});
 		pu.rule("Expr -> Expr [ Type ]", () -> {
 			// TAPP
 			Term term = (Term) pu.get(1);
 			Type ty = (Type) pu.get(3);
-			
+
 			return new TApp(term, ty);
 		});
-		pu.rule("Expr -> Cond", () -> { return pu.get(1); });
+		pu.rule("Expr -> Cond", () -> {
+			return pu.get(1);
+		});
 
-		pu.rule("Cond -> LogicNot", () -> { return pu.get(1); });
+		pu.rule("Cond -> LogicNot", () -> {
+			return pu.get(1);
+		});
 		pu.rule("LogicNot -> ! LogicOr", () -> {
 			return new ExprTerm((Term) pu.get(2), ExprTerm.NOT);
 		});
-		pu.rule("LogicNot -> LogicOr", () -> { return pu.get(1); });
+		pu.rule("LogicNot -> LogicOr", () -> {
+			return pu.get(1);
+		});
 		pu.rule("LogicOr -> LogicOr or LogicAnd", () -> {
 			return new ExprTerm((Term) pu.get(1), ExprTerm.OR, (Term) pu.get(3));
 		});
-		pu.rule("LogicOr -> LogicAnd", () -> { return pu.get(1); });
+		pu.rule("LogicOr -> LogicAnd", () -> {
+			return pu.get(1);
+		});
 		pu.rule("LogicAnd -> LogicAnd and CompEqNeq", () -> {
 			return new ExprTerm((Term) pu.get(1), ExprTerm.AND, (Term) pu.get(3));
 		});
-		pu.rule("LogicAnd -> CompEqNeq", () -> { return pu.get(1); });
+		pu.rule("LogicAnd -> CompEqNeq", () -> {
+			return pu.get(1);
+		});
 
 		pu.rule("CompEqNeq -> CompEqNeq == Comp", () -> {
 			return new ExprTerm((Term) pu.get(1), ExprTerm.EQUAL, (Term) pu.get(3));
@@ -133,7 +190,9 @@ public class Parser {
 		pu.rule("CompEqNeq -> CompEqNeq != Comp", () -> {
 			return new ExprTerm((Term) pu.get(1), ExprTerm.NOTEQUAL, (Term) pu.get(3));
 		});
-		pu.rule("CompEqNeq -> Comp", () -> { return pu.get(1); });
+		pu.rule("CompEqNeq -> Comp", () -> {
+			return pu.get(1);
+		});
 		pu.rule("Comp -> Comp < ArithAddSub", () -> {
 			return new ExprTerm((Term) pu.get(1), ExprTerm.LTHAN, (Term) pu.get(3));
 		});
@@ -146,7 +205,9 @@ public class Parser {
 		pu.rule("Comp -> Comp >= ArithAddSub", () -> {
 			return new ExprTerm((Term) pu.get(1), ExprTerm.GEQUAL, (Term) pu.get(3));
 		});
-		pu.rule("Comp -> ArithAddSub", () -> { return pu.get(1); });
+		pu.rule("Comp -> ArithAddSub", () -> {
+			return pu.get(1);
+		});
 
 		pu.rule("ArithAddSub -> ArithAddSub + ArithMulDiv", () -> {
 			return new ExprTerm((Term) pu.get(1), ExprTerm.ADD, (Term) pu.get(3));
@@ -154,31 +215,49 @@ public class Parser {
 		pu.rule("ArithAddSub -> ArithAddSub - ArithMulDiv", () -> {
 			return new ExprTerm((Term) pu.get(1), ExprTerm.SUB, (Term) pu.get(3));
 		});
-		pu.rule("ArithAddSub -> ArithMulDiv", () -> { return pu.get(1); });
+		pu.rule("ArithAddSub -> ArithMulDiv", () -> {
+			return pu.get(1);
+		});
 		pu.rule("ArithMulDiv -> ArithMulDiv * ArithUnary", () -> {
 			return new ExprTerm((Term) pu.get(1), ExprTerm.MUL, (Term) pu.get(3));
 		});
 		pu.rule("ArithMulDiv -> ArithMulDiv / ArithUnary", () -> {
 			return new ExprTerm((Term) pu.get(1), ExprTerm.DIV, (Term) pu.get(3));
 		});
-		pu.rule("ArithMulDiv -> ArithUnary", () -> { return pu.get(1); });
+		pu.rule("ArithMulDiv -> ArithUnary", () -> {
+			return pu.get(1);
+		});
 		pu.rule("ArithUnary -> - Term", () -> {
 			return new ExprTerm((Term) pu.get(2), ExprTerm.UNARY);
 		});
-		pu.rule("ArithUnary -> Term", () -> { return pu.get(1); });
+		pu.rule("ArithUnary -> Term", () -> {
+			return pu.get(1);
+		});
 
-		pu.rule("Term -> id", () -> { return new Var(pu.getText(1)); });
-		pu.rule("Term -> num", () -> { return new Num(Integer.parseInt(pu.getText(1))); });
-		pu.rule("Term -> str", () -> { return new Str(pu.getText(1)); });
-		pu.rule("Term -> bool", () -> { return new Bool(pu.getText(1)); });
-		pu.rule("Term -> ( )", () -> { return new Unit(); });
-		pu.rule("Term -> ( LExpr )", () -> { return pu.get(2); });
-		
+		pu.rule("Term -> id", () -> {
+			return new Var(pu.getText(1));
+		});
+		pu.rule("Term -> num", () -> {
+			return new Num(Integer.parseInt(pu.getText(1)));
+		});
+		pu.rule("Term -> str", () -> {
+			return new Str(pu.getText(1));
+		});
+		pu.rule("Term -> bool", () -> {
+			return new Bool(pu.getText(1));
+		});
+		pu.rule("Term -> ( )", () -> {
+			return new Unit();
+		});
+		pu.rule("Term -> ( LExpr )", () -> {
+			return pu.get(2);
+		});
+
 		pu.rule("Type -> ForAllType LocArrow Type", () -> {
 			Type argTy = (Type) pu.get(1);
 			Location loc = (Location) pu.get(2);
 			Type retTy = (Type) pu.get(3);
-			
+
 			if (loc == Location.Polymorphic) {
 				return new FunType(argTy, new LocVarType(n++), retTy);
 			}
@@ -188,18 +267,18 @@ public class Parser {
 		pu.rule("Type -> ForAllType", () -> {
 			return pu.get(1);
 		});
-		
+
 		pu.rule("ForAllType -> forall id . ForAllType", () -> {
 			String id = pu.getText(2);
 			VarType tyId = new VarType(id);
 			Type ty = (Type) pu.get(4);
-			
+
 			return new ForAll(tyId, ty);
 		});
 		pu.rule("ForAllType -> PrimaryType", () -> {
 			return pu.get(1);
 		});
-		
+
 		pu.rule("PrimaryType -> Unit", () -> {
 			return new UnitType();
 		});
@@ -208,7 +287,7 @@ public class Parser {
 		});
 		pu.rule("PrimaryType -> Bool", () -> {
 			return new BoolType();
-		}); 
+		});
 		pu.rule("PrimaryType -> String", () -> {
 			return new StrType();
 		});
@@ -218,10 +297,10 @@ public class Parser {
 		pu.rule("PrimaryType -> ( Type )", () -> {
 			return pu.get(2);
 		});
-		
+
 		pu.rule("LocArrow -> - id ->", () -> {
 			Location loc = getLoc(pu.getText(2));
-			
+
 			return loc;
 		});
 		pu.rule("LocArrow -> ->", () -> {
@@ -240,9 +319,46 @@ public class Parser {
 			return null;
 		});
 	}
-	
+
 	public Term Parsing(Reader r) throws ParserException, IOException, LexerException {
-		return (Term) pu.Parsing(r);
+		ArrayList<TripleTup<String, Type, Term>> list = (ArrayList<TripleTup<String, Type, Term>>) pu.Parsing(r);
+		TripleTup<String, Type, Term> mainTerm = null;
+
+		for (int i = list.size() - 1; i >= 0; i--) {
+			TripleTup<String, Type, Term> t = list.get(i);
+
+			if (t.getFirst().equals("main")) {
+				mainTerm = t;
+				list.remove(i);
+				break;
+			}
+		}
+
+		try {
+			if (mainTerm == null)
+				throw new ParserException("Program must have main function");
+
+			else {
+				Term main = mainTerm.getThird();
+				Term ret = main;
+
+				for (int i = list.size() - 1; i >= 0; i--) {
+					TripleTup<String, Type, Term> t = list.get(i);
+
+					if (t.getSecond() != null)
+						ret = new Let(t.getFirst(), t.getSecond(), t.getThird(), ret);
+					else
+						ret = new Let(t.getFirst(), t.getThird(), ret);
+				}
+
+				return ret;
+			}
+		}
+		catch (ParserException e) {
+			e.printStackTrace();
+			return null;
+		}
+//		return (Term) pu.Parsing(r);
 	}
 
 	private Location getLoc(String loc) {
